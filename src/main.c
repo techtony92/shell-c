5a8b59007edbdd47cd4c04c258a35ac5f68f7f0c
tony <techtony92@gmail.com>
2026-01-04 00:50:13 -0500

    testing REPL

--- src/main.c
+++ src/main.c
@@ -7,10 +7,12 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-   printf("$ ");
-   char command[COMMAND_BUFFER];
-   fgets(command, sizeof(command), stdin);
-   command[strcspn(command, "\n")] = '\0';
-   printf("%s: command not found", command);
+  for(;;){
+    printf("$ ");
+    char command[COMMAND_BUFFER];
+    fgets(command, sizeof(command), stdin);
+    command[strcspn(command, "\n")] = '\0';
+    printf("%s: command not found", command);
+  }
   return 0;
 }
