491d68717dda5f15273373c6a4badda196aab9e3
tony <techtony92@gmail.com>
2026-01-03 22:52:12 -0500

    testing_command_input

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define COMMAND_BUFFER 1024
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
   // TODO: Uncomment the code below to pass the first stage
    printf("$ ");
-
+   char command[COMMAND_BUFFER];
+   fgets(command, sizeof(command), stdin);
+   printf("%s: Command Not Found", command);
   return 0;
 }
