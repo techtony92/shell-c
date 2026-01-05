1f4049f7ec52b97ffd8c8d8d5dc06a39e8c86297
tony <techtony92@gmail.com>
2026-01-05 00:30:59 -0500

    Apply Solution to failure

--- src/main.c
+++ src/main.c
@@ -4,20 +4,31 @@
 
 #define COMMAND_BUFFER 1024
 int main(int argc, char *argv[]) {
-  // Flush after every printf
-  setbuf(stdout, NULL);
-  int c;
-  for(;;){
+  while(1){
+    // Flush after every printf
+    setbuf(stdout, NULL);
     printf("$ ");
     char command[COMMAND_BUFFER];
     fgets(command, sizeof(command), stdin);
     command[strcspn(command, "\n")] = '\0';
-    while((c = getchar()) != EOF && c != '\n'){
-        if(&c == "exit"){
-          exit(0);
-        }
+    size_t len = strlen(command);
+
+    if(len > 0 && command[len - 1] == '\n'){
+      command[len - 1] = '\0';
+    }
+    // while(1){
+    //   // Check if: quit/exit
+    //   if(input == "quit" || input == "exit"){
+    //     return 0;
+    //   }
+    // }
+    
+
+    if(strcmp(command, "quit") == 0 || strcmp(command, "exit") == 0){
+      return 0;
     }
     printf("%s: command not found \n", command);
   }
+  
   return 0;
 }
