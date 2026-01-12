bb7a25e0aedc77b8d8508cf39c653e634f09e35d
tony <techtony92@gmail.com>
2026-01-12 05:40:08 -0500

    testing fix for failure: implement echo

--- src/main.c
+++ src/main.c
@@ -3,12 +3,15 @@
 #include <string.h>
 
 #define COMMAND_BUFFER 1024
+#define ECHO 5
+
 int main(int argc, char *argv[]) {
+  char echo[ECHO];
+  char command[COMMAND_BUFFER];
   while(1){
     // Flush after every printf
     setbuf(stdout, NULL);
     printf("$ ");
-    char command[COMMAND_BUFFER];
     fgets(command, sizeof(command), stdin);
     command[strcspn(command, "\n")] = '\0';
     size_t len = strlen(command);
@@ -22,8 +25,26 @@ int main(int argc, char *argv[]) {
     //     return 0;
     //   }
     // }
-    
+   // Check for echo
+    for(int i = 0; i < ECHO; i++){
+      echo[i] = command[i];
+      if(i == 3){
+        echo[ECHO -1] = '\0';
+        break;
+      }
+    }
 
+    
+    if(strcmp(echo, "echo") == 0){
+      for(int i = ECHO; i <= len; i++){
+       
+        
+        command[i - ECHO] = command[i];
+      }
+      command[len - 1] = '\0';
+
+    }
+    
     if(strcmp(command, "quit") == 0 || strcmp(command, "exit") == 0){
       return 0;
     }
